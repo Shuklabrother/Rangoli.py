0b9dd90a5a7b8410251dd1594703f63db1763b2f
Code with Shukla Brother <90129105+Shuklabrother@users.noreply.github.com>
2023-04-20 13:28:36 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,21 @@
+from turtle import * color('red', 'yellow') begin_fill() while True: forward(200) left(170) if abs(pos()) < 1: break end_fill() done()from turtle import * 
+
+color('red', 'yellow') 
+
+begin_fill() 
+
+while True:
+
+	forward(200) 
+
+	speed(0)
+
+	left(170) 
+
+	if abs(pos()) < 1:
+
+		 break 
+
+end_fill()
+
+done()
